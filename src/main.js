5f49c840eaa75898186d930a9fb4ea816db3b9a1
Oluwwatimilehin Awoniyi <awoniyitimi@outlook.com>
2022-06-22 00:11:49 +0100

    Fetched API and linked data to HTML

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
 const dice = document.querySelector(".dice");
 const adviceBox = document.querySelector(".advice");
 const adviceIndex = document.querySelector(".advice-id");
+
+const getAdvice = async () => {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const data = await response.json();
+  // console.log(data);
+  // console.log(data.slip.id);
+  // console.log(data.slip.advice);
+
+  // console.log(response);
+  return data;
+};
+
+dice.addEventListener("click", () => {
+  getAdvice()
+    .then((data) => {
+      adviceBox.textContent = data.slip.advice;
+      adviceIndex.textContent = `Advice #${data.slip.id}`;
+    })
+    .catch((err) => err);
+});
