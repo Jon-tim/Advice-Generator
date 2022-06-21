8a97d9182c7ac59f329af96847c5cc4a05612448
Oluwwatimilehin Awoniyi <awoniyitimi@outlook.com>
2022-06-21 08:50:44 +0100

    updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+const { cyan } = require("color-name");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      "light-cyan": "hsl(193, 38%, 86%)",
+      "neon-green": "hsl(150, 100%, 66%)",
+      "grayish-blue": "hsl(217, 19%, 38%)",
+      "dark-grayish-blue": "hsl(217, 19%, 24%)",
+      "dark-blue": "hsl(218, 23%, 16%)",
+    },
+    fontSize: {
+      quoteFont: "28px",
+    },
+    fontFamily: {
+      "sans-serif": ["Manrope", "sans - serif"],
+    },
+    fontWeight: {
+      normal: "400",
+      bold: "800",
+    },
   },
   plugins: [],
 };
