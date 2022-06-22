6984f4515208f4a2c0b86a93090b45b3363025b0
Oluwwatimilehin Awoniyi <awoniyitimi@outlook.com>
2022-06-22 03:27:45 +0100

    Update Javascript

--- src/main.js
+++ src/main.js
@@ -1,23 +1,13 @@
 const dice = document.querySelector(".dice");
 const adviceBox = document.querySelector(".advice");
-const adviceIndex = document.querySelector(".advice-id");
+const adviceId = document.querySelector(".advice-id");
 
 const getAdvice = async () => {
   const response = await fetch("https://api.adviceslip.com/advice");
   const data = await response.json();
-  // console.log(data);
-  // console.log(data.slip.id);
-  // console.log(data.slip.advice);
-
-  // console.log(response);
-  return data;
+  console.log(data);
+  adviceBox.textContent = `"${data.slip.advice}"`;
+  adviceId.textContent = `advice #${data.slip.id}`;
 };
 
-dice.addEventListener("click", () => {
-  getAdvice()
-    .then((data) => {
-      adviceBox.textContent = `"${data.slip.advice}"`;
-      adviceIndex.textContent = `Advice #${data.slip.id}`;
-    })
-    .catch((err) => err);
-});
+dice.addEventListener("click", getAdvice);
